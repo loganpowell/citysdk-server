d86fdb9801bee346d5783995b12326a7840a97e9
Logan Powell <loganp@tepper.cmu.edu>
2019-05-28 16:52:32 -0400

    wrapping in https and abdicating PORT

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-let census = require('citysdk')
-let http = require('http')
-let { json } = require('micro')
+const census = require('citysdk')
+const https = require('https')
+const { json, run, send } = require('micro')
 
-let censusPromise = args => {
+const {key, cert, passphrase} = require('openssl-self-signed-certificate');
+
+const PORT = process.env.PORT || 8080
+
+const censusPromise = args => {
   return new Promise((resolve, reject) => {
     census(args, (err, data) => {
       if (!err) {
@@ -18,12 +22,17 @@ let censusPromise = args => {
   });
 };
 
-const server = new http.Server(micro( async (req, res) => {
+
+const options = {key, cert, passphrase};
+
+const microHttps = fn => https.createServer(options, (req, res) => run(req, res, fn))
+
+const server = microHttps(async (req, res) => {
     const query = await json(req)
     let answer = await censusPromise(query)
-    console.log(answer)
-    res.end(answer)
-}))
+    console.log("answer: " + answer)
+    send(answer, 200, {encrypted: req.client.encrypted});
+})
 
-server.listen(8080)
+server.listen(PORT)
 
