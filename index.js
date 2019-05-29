2265a53b10b0b28952f0fd4f82e03b6e49de910b
Logan Powell <loganp@tepper.cmu.edu>
2019-05-29 10:37:42 -0400

    working on local, testing on cloud

--- index.js
+++ index.js
@@ -1,20 +1,36 @@
 const census = require('citysdk')
-const https = require('https')
-const { json, run, send } = require('micro')
+const micro = require('micro')
+const {
+  json,
+  send,
+  sendError
+} = micro
 
-const {key, cert, passphrase} = require('openssl-self-signed-certificate');
+const { router, post} = require('microrouter')
 
 const PORT = process.env.PORT || 8080
+console.log("PORT: " + PORT)
+
+// module.exports = () => "welcome to micro"
+//
+
+/*
+    "start": "cross-env NODE_OPTIONS=--max-old-space-size=4096 micro -p 8080",
+    "dev": "cross-env NODE_OPTIONS=--max-old-space-size=4096 micro-dev -p 8080"
+ */
 
 const censusPromise = args => {
+  console.log("In censusPromise...")
   return new Promise((resolve, reject) => {
     census(args, (err, data) => {
       if (!err) {
+        console.log("Success!")
         resolve(data);
       } else if (err) {
-        console.log(err)
+        console.log("CitySDK Error: " + err)
         resolve(err);
       } else {
+        console.log("SERVER ERROR:")
         console.log(err)
         reject(err)
       }
@@ -22,17 +38,62 @@ const censusPromise = args => {
   });
 };
 
+//
+// const getHandler = async () => `Welcome to CitySDK.
+// To use this server, make a POST request with
+// 1. header: "Content-Type" : "application/json"
+// 2. your CitySDK arguments in the body of the request
+// Docs: https://github.com/uscensusbureau/citysdk"
+// `
+// const postHandler = async args => {
+//   const data = await censusPromise(args)
+//   return data
+// }
+//
+// async function methodHandler(request, response) {
+//   try {
+//     switch (request.method) {
+//       case 'POST':
+//         return await postHandler(request);
+//       case 'GET':
+//         return await getHandler();
+//       default:
+//         send(response, 405, 'Invalid method');
+//         break;
+//     }
+//   } catch (error) {
+//     throw error;
+//   }
+// }
 
-const options = {key, cert, passphrase};
+// doesn't work in Cloud.gov (needs to take port from env)
+module.exports = router(
+  post('/', async (req, res) => {
+    const args = await json(req)
+    const data = await censusPromise(args)
+    send(res, 200, data)
+  })
+)
 
-const microHttps = fn => https.createServer(options, (req, res) => run(req, res, fn))
+// testing...
+// module.exports = router(
+//   post('/', async (req, res) => {
+//     const data = await json(req)
+//     send(res, 200, data)
+//   })
+// )
 
-const server = microHttps(async (req, res) => {
-    const query = await json(req)
-    let answer = await censusPromise(query)
-    console.log("answer: " + answer)
-    send(answer, 200, {encrypted: req.client.encrypted});
-})
+//
+// const server = micro(async (request, response) => {
+//   const args = await json(request)
+//   try {
+//     send(response, 200, await methodHandler(args));
+//   } catch (error) {
+//     sendError(request, response, error);
+//   }
+// });
+//
+// server.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
 
-server.listen(PORT)
+
 
